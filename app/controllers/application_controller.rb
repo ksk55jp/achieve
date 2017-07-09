846b26c1d05d6cbaafda86ce0d3c2dbfa670c118
Keisuke san <ksk55jp@gmail.com>
2017-07-09 21:02:49 +0900

    added login session control

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,19 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  #Execute method in before_action
+  before_action :configure_permitted_parameters, if: :devise_controller?
+   
+  #Substitute array into variable PERMISSIBLE_ATTRIBUTES
+  PERMISSIBLE_ATTRIBUTES = %i(name)
+  
+  protected
+
+  # define method that adds columns tinto strong parameter in devise
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: PERMISSIBLE_ATTRIBUTES) 
+    devise_parameter_sanitizer.permit(:account_update, keys: PERMISSIBLE_ATTRIBUTES) 
+  end 
+
 end
